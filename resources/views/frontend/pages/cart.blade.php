3bbfffe0ff05ff40380fa56de28966e5978b3108
Kahraman Karadavut <kkaradavut.3@gmail.com>
2023-08-20 14:55:31 +0300

    a

--- resources/views/frontend/pages/cart.blade.php
+++ resources/views/frontend/pages/cart.blade.php
@@ -4,7 +4,7 @@
 <div class="bg-light py-3">
     <div class="container">
       <div class="row">
-        <div class="col-md-12 mb-0"><a href="index.html">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Cart</strong></div>
+        <div class="col-md-12 mb-0"><a href="{{ route('mainPage') }}">Anasayfa</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Sepet</strong></div>
       </div>
     </div>
   </div>
@@ -17,12 +17,12 @@
             <table class="table table-bordered">
               <thead>
                 <tr>
-                  <th class="product-thumbnail">Image</th>
-                  <th class="product-name">Product</th>
-                  <th class="product-price">Price</th>
-                  <th class="product-quantity">Quantity</th>
-                  <th class="product-total">Total</th>
-                  <th class="product-remove">Remove</th>
+                  <th class="product-thumbnail">Resim</th>
+                  <th class="product-name">Ürün</th>
+                  <th class="product-price">Fiyat</th>
+                  <th class="product-quantity">Adet</th>
+                  <th class="product-total">Toplam</th>
+                  <th class="product-remove">Sil</th>
                 </tr>
               </thead>
               <tbody>
@@ -83,22 +83,22 @@
         <div class="col-md-6">
           <div class="row mb-5">
             <div class="col-md-6 mb-3 mb-md-0">
-              <button class="btn btn-primary btn-sm btn-block">Update Cart</button>
+              <button class="btn btn-primary btn-sm btn-block">Sepetİ Güncelle</button>
             </div>
             <div class="col-md-6">
-              <button class="btn btn-outline-primary btn-sm btn-block">Continue Shopping</button>
+              <button class="btn btn-outline-primary btn-sm btn-block">Alışverİşe Devam Et</button>
             </div>
           </div>
           <div class="row">
             <div class="col-md-12">
-              <label class="text-black h4" for="coupon">Coupon</label>
-              <p>Enter your coupon code if you have one.</p>
+              <label class="text-black h4" for="coupon">Kupon</label>
+              <p>Eğer kupon kodunuz varsa kullanın.</p>
             </div>
             <div class="col-md-8 mb-3 mb-md-0">
-              <input type="text" class="form-control py-3" id="coupon" placeholder="Coupon Code">
+              <input type="text" class="form-control py-3" id="coupon" placeholder="Kupon Kodu">
             </div>
             <div class="col-md-4">
-              <button class="btn btn-primary btn-sm">Apply Coupon</button>
+              <button class="btn btn-primary btn-sm">Kodu Uygula</button>
             </div>
           </div>
         </div>
@@ -107,12 +107,12 @@
             <div class="col-md-7">
               <div class="row">
                 <div class="col-md-12 text-right border-bottom mb-5">
-                  <h3 class="text-black h4 text-uppercase">Cart Totals</h3>
+                  <h3 class="text-black h4 text-uppercase">Sepet Tutarı</h3>
                 </div>
               </div>
               <div class="row mb-3">
                 <div class="col-md-6">
-                  <span class="text-black">Subtotal</span>
+                  <span class="text-black">Toplam</span>
                 </div>
                 <div class="col-md-6 text-right">
                   <strong class="text-black">$230.00</strong>
@@ -120,7 +120,7 @@
               </div>
               <div class="row mb-5">
                 <div class="col-md-6">
-                  <span class="text-black">Total</span>
+                  <span class="text-black">Toplam Tutar</span>
                 </div>
                 <div class="col-md-6 text-right">
                   <strong class="text-black">$230.00</strong>
